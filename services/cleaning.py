4206d206f7a5b4d104514b040eb487f416202d94
akshay7546 <akshaykumarpakra853204@gmail.com>
2026-06-21 19:21:02 +0530

    Fixed cleaning logic

--- services/cleaning.py
+++ services/cleaning.py
@@ -15,14 +15,19 @@ def clean_data(file_path):
     # Fill Missing Values
     for col in df.columns:
 
-        if df[col].dtype == 'object':
+        if pd.api.types.is_numeric_dtype(df[col]):
 
-            if not df[col].mode().empty:
-                df[col] = df[col].fillna(df[col].mode()[0])
+            df[col] = df[col].fillna(
+                df[col].mean()
+            )
 
         else:
 
-            df[col] = df[col].fillna(df[col].mean())
+            if not df[col].mode().empty:
+
+                df[col] = df[col].fillna(
+                    df[col].mode()[0]
+                )
 
     cleaned_folder = "uploads/cleaned_data"
 
@@ -38,48 +43,5 @@ def clean_data(file_path):
     )
 
     df.to_csv(cleaned_path, index=False)
-
+    
     return cleaned_path
-
-
-
-
-# import pandas as pd
-# import os
-
-# def clean_data(file_path):
-
-#     if file_path.endswith('.csv'):
-#         df = pd.read_csv(file_path)
-
-#     else:
-#         df = pd.read_excel(file_path)
-
-#     # Remove Duplicates
-#     df.drop_duplicates(inplace=True)
-
-#     # Fill Missing Values
-#     for col in df.columns:
-
-#         if df[col].dtype == 'object':
-
-#             if not df[col].mode().empty:
-#                 df[col] = df[col].fillna(df[col].mode()[0])
-
-#         else:
-
-#             df[col] = df[col].fillna(df[col].mean())
-
-#     cleaned_folder = "uploads/cleaned_data"
-
-#     if not os.path.exists(cleaned_folder):
-#         os.makedirs(cleaned_folder)
-
-#     cleaned_path = os.path.join(
-#         cleaned_folder,
-#         "cleaned_dataset.csv"
-#     )
-
-#     df.to_csv(cleaned_path, index=False)
-
-#     return cleaned_path
